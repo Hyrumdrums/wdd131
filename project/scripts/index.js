c875f04d212e52109cfbe8912dbe1c8b851c74d1
Dallin Layton <hyrumdrums@gmail.com>
2025-12-06 16:05:19 -0700

    add boilerplate

---
+++ project/scripts/index.js
@@ -0,0 +1,6 @@
+// Get current year and last modified date
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
